e16e9e9f191c62f4184e680aff510b326667c6e7
Zach Daniel <zach@zachdaniel.dev>
2025-03-12 22:36:12 -0400

    chore: set required inputs, and properly process errors

--- lib/ash_ai.ex
+++ lib/ash_ai.ex
@@ -7,6 +7,8 @@ defmodule AshAi do
 
   defstruct []
 
+  require Logger
+
   @full_text %Spark.Dsl.Section{
     name: :full_text,
     imports: [Ash.Expr],
@@ -238,7 +240,9 @@ defmodule AshAi do
           %{
             input: %{
               type: :object,
-              properties: attrs
+              properties: attrs,
+              required:
+                AshJsonApi.OpenApi.required_write_attributes(resource, action.arguments, action)
             }
           }
           |> add_action_specific_properties(resource, action)
@@ -256,10 +260,12 @@ defmodule AshAi do
       action.description ||
         "Call the #{action.name} action on the #{inspect(resource)} resource"
 
+    parameter_schema = parameter_schema(domain, resource, action)
+
     LangChain.Function.new!(%{
       name: name,
       description: description,
-      parameters_schema: parameter_schema(domain, resource, action),
+      parameters_schema: parameter_schema,
       function: fn arguments, context ->
         actor = context[:actor]
 
@@ -392,15 +398,121 @@ defmodule AshAi do
         rescue
           error ->
             {:error,
-             Jason.encode!(
-               serialize_errors(
-                 AshJsonApi.Error.to_json_api_errors(domain, resource, error, action.type)
-               )
-             )}
+             domain
+             |> AshJsonApi.Error.to_json_api_errors(resource, error, action.type)
+             |> serialize_errors()
+             |> Jason.encode!()}
         end
       end
     })
   end
+
+  def to_json_api_errors(domain, resource, errors, type) when is_list(errors) do
+    Enum.flat_map(errors, &to_json_api_errors(domain, resource, &1, type))
+  end
+
+  def to_json_api_errors(domain, resource, %mod{errors: errors}, type)
+      when mod in [Forbidden, Framework, Invalid, Unknown] do
+    Enum.flat_map(errors, &to_json_api_errors(domain, resource, &1, type))
+  end
+
+  def to_json_api_errors(_domain, _resource, %AshJsonApi.Error{} = error, _type) do
+    [error]
+  end
+
+  def to_json_api_errors(domain, resource, %{class: :invalid} = error, type) do
+    if AshJsonApi.ToJsonApiError.impl_for(error) do
+      error
+      |> AshJsonApi.ToJsonApiError.to_json_api_error()
+      |> List.wrap()
+      |> Enum.flat_map(&with_source_pointer(&1, error))
+    else
+      uuid = Ash.UUID.generate()
+
+      stacktrace =
+        case error do
+          %{stacktrace: %{stacktrace: v}} ->
+            v
+
+          _ ->
+            nil
+        end
+
+      Logger.warning(
+        "`#{uuid}`: AshJsonApi.Error not implemented for error:\n\n#{Exception.format(:error, error, stacktrace)}"
+      )
+
+      if AshJsonApi.Domain.Info.show_raised_errors?(domain) do
+        [
+          %AshJsonApi.Error{
+            id: uuid,
+            status_code: class_to_status(error.class),
+            code: "something_went_wrong",
+            title: "SomethingWentWrong",
+            detail: """
+            Raised error: #{uuid}
+
+            #{Exception.format(:error, error, stacktrace)}"
+            """
+          }
+        ]
+      else
+        [
+          %AshJsonApi.Error{
+            id: uuid,
+            status_code: class_to_status(error.class),
+            code: "something_went_wrong",
+            title: "SomethingWentWrong",
+            detail: "Something went wrong. Error id: #{uuid}"
+          }
+        ]
+      end
+    end
+  end
+
+  def to_json_api_errors(_domain, _resource, %{class: :forbidden} = error, _type) do
+    [
+      %AshJsonApi.Error{
+        id: Ash.UUID.generate(),
+        status_code: class_to_status(error.class),
+        code: "forbidden",
+        title: "Forbidden",
+        detail: "forbidden"
+      }
+    ]
+  end
+
+  def to_json_api_errors(_domain, _resource, error, _type) do
+    uuid = Ash.UUID.generate()
+
+    stacktrace =
+      case error do
+        %{stacktrace: %{stacktrace: v}} ->
+          v
+
+        _ ->
+          nil
+      end
+
+    Logger.warning(
+      "`#{uuid}`: AshJsonApi.Error not implemented for error:\n\n#{Exception.format(:error, error, stacktrace)}"
+    )
+
+    [
+      %AshJsonApi.Error{
+        id: uuid,
+        status_code: class_to_status(error.class),
+        code: "something_went_wrong",
+        title: "SomethingWentWrong",
+        detail: "Something went wrong. Error id: #{uuid}"
+      }
+    ]
+  end
+
+  @doc "Turns an error class into an HTTP status code"
+  def class_to_status(:forbidden), do: 403
+  def class_to_status(:invalid), do: 400
+  def class_to_status(_), do: 500
 
   defp serialize_errors(errors) do
     errors
@@ -416,6 +528,33 @@ defmodule AshAi do
       |> add_if_defined([:source, :parameter], error.source_parameter)
       |> add_if_defined(:meta, parse_error(error.meta))
     end)
+  end
+
+  def with_source_pointer(%{source_pointer: source_pointer} = built_error, _)
+      when source_pointer not in [nil, :undefined] do
+    [built_error]
+  end
+
+  def with_source_pointer(built_error, %{fields: fields, path: path})
+      when is_list(fields) and fields != [] do
+    Enum.map(fields, fn field ->
+      %{built_error | source_pointer: source_pointer(field, path)}
+    end)
+  end
+
+  def with_source_pointer(built_error, %{field: field, path: path})
+      when not is_nil(field) do
+    [
+      %{built_error | source_pointer: source_pointer(field, path)}
+    ]
+  end
+
+  def with_source_pointer(built_error, _) do
+    [built_error]
+  end
+
+  defp source_pointer(field, path) do
+    "/input/#{Enum.join(List.wrap(path) ++ [field], "/")}"
   end
 
   defp add_if_defined(params, _, :undefined) do
