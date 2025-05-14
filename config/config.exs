cf855984a4201d2240dee46c1914add49982127f
Zach Daniel <zach@zachdaniel.dev>
2025-05-14 07:24:42 +0200

    chore: project setup

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,19 @@ import Config
 if Mix.env() == :test do
   import_config "test.exs"
 end
+
+if Mix.env() == :dev do
+  config :git_ops,
+    mix_project: AshAi.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/ash-project/ash_ai",
+    # Instructs the tool to manage your mix version in your `mix.exs` file
+    # See below for more information
+    manage_mix_version?: true,
+    # Instructs the tool to manage the version in your README.md
+    # Pass in `true` to use `"README.md"` or a string to customize
+    manage_readme_version: [
+      "README.md"
+    ],
+    version_tag_prefix: "v"
+end
